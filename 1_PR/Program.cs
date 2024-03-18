06bd44a40f59c33cf90b6b23c0e56a9189bc29aa
Vital <rediko26@gmail.com>
2024-03-18 17:15:15 +0300

    pervoe

---
+++ 1_PR/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PR
+{
+    class Sobaka
+    {
+        public string klichka { get; set; }
+        public string poroda { get; set; }
+        public float vozrast { get; set; }
+        public Sobaka(string klichka, string poroda, float vozrast)
+        {
+            this.klichka = klichka;
+            this.poroda = poroda;
+            this.vozrast = vozrast;
+        }
+        //static void Main(string[] args)
+        //{
+
+        //}
+    }
+}
